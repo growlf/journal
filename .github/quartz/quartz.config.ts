94d5f5592b95e30ac0e3e629836438b8f4ea7280
Garth Johnson <growlfd@gmail.com>
2025-08-03 18:45:47 -0700

    try2

--- .github/quartz/quartz.config.ts
+++ .github/quartz/quartz.config.ts
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Sixtyfour Convergence",
+        body: "Space Grotesk",
         code: "IBM Plex Mono",
       },
       colors: {
@@ -43,8 +43,8 @@ const config: QuartzConfig = {
           gray: "#ECEFF4",
           darkgray: "#D8DEE9",
           dark: "#ECEFF4",
-          secondary: "#5E81AC",
-          tertiary: "#B48EAD",
+          secondary: "#3870b4ff",
+          tertiary: "#968eb4ff",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
       },
