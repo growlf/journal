5d372a3fa4478fba58326b7b5b8a67d75c32c9d4
Garth Johnson <growlfd@gmail.com>
2025-08-15 17:46:55 -0700

    Obsidian edits from Gearbox at 2025-08-15 17:46:55
    Affected files:
    .github/quartz/custom.scss
    .github/quartz/quartz.config.ts

--- .github/quartz/quartz.config.ts
+++ .github/quartz/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "growlf.github.io/journal",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "_templates", "_assets", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
