96e2032acf0a9f4e094cbd8162b8243d92ba2c92
Garth Johnson <growlfd@gmail.com>
2025-10-20 20:25:00 -0700

    Obsidian edits from  at 2025-10-20 20:25:00
    Affected files:
    .github/quartz/quartz.config.ts

--- .github/quartz/quartz.config.ts
+++ .github/quartz/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "growlf.github.io/journal",
-    ignorePatterns: ["private", "_templates", ".obsidian"],
+    ignorePatterns: ["private", "_templates", ".obsidian", "_assets"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
