422cc368ce11ac9fd3c764907054a21b2e952e6c
NetYeti <growlfd@gmail.com>
2026-04-14 02:22:24 -0700

    new theme

--- .github/quartz/quartz.layout.ts
+++ .github/quartz/quartz.layout.ts
@@ -8,6 +8,21 @@ export const sharedPageComponents: SharedLayout = {
     Component.PageTitle(),
     Component.Search(),
     Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer({
+      title: "Menu",
+      useSavedState: true,
+      sortFn: (a, b) => {
+        if ((!a.file && !b.file) || (a.file && b.file)) {
+          return a.displayName.localeCompare(b.displayName)
+        }
+        if (a.file && !b.file) return 1
+        else return -1
+      },
+      filterFn: (node) => {
+        const omit = new Set(["_assets", "internal"])
+        return !omit.has(node.displayName?.toLowerCase() ?? "")
+      },
+    })),
   ],
   afterBody: [],
   footer: Component.Footer({
@@ -17,15 +32,6 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
-// Explorer component configuration
-const explorer = Component.Explorer({
-  filterFn: (node) => {
-    // set containing names of everything you want to filter out
-    const omit = new Set(["_assets", "internal", "network"])
-    return !omit.has(node.displayName?.toLowerCase() ?? "")
-  },
-})
-
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
@@ -34,9 +40,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  left: [
-    Component.DesktopOnly(explorer),
-  ],
+  left: [], // Sidebars are now purely for forest atmosphere and supplemental info
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
@@ -47,8 +51,6 @@ export const defaultContentPageLayout: PageLayout = {
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.DesktopOnly(explorer),
-  ],
+  left: [],
   right: [],
 }
